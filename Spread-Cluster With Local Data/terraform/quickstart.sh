21d8926f5f7fbe2383f15388813ef0e20ef124ce
Jan Peschke <jan.peschke@quobyte.com>
2023-02-22 14:06:26 +0100

    correct wrong path

--- Spread-Cluster With Local Data/terraform/quickstart.sh
+++ Spread-Cluster With Local Data/terraform/quickstart.sh
@@ -10,5 +10,5 @@ git clone https://github.com/jan379/quobyte-presales.git
 grep "fill in a valid Quobyte license key" ansible-vars && exit 1
 cp ansible-vars quobyte-ansible/vars/ansible-vars
 cp ansible-inventory.yaml quobyte-ansible/inventory.yaml
-cd provisioning
+cd quobyte-ansible 
 ansible-playbook -i inventory.yaml 00_install_quobyte_server.yaml 01_setup_coreservices.yaml 02_create_superuser.yaml 03_add_metadataservices.yaml 04_add_dataservices.yaml 05_optional_tune-cluster.yaml 06_optional_install_defaultclient.yaml 07_optional_license_cluster.yaml
