9c8aa7a1ebb23bde902588e1d967d39186f6ea17
Jan Peschke <jan.peschke@quobyte.com>
2023-02-24 11:29:20 +0100

    remove hard coded git name and email, fix a typo

--- Spread-Cluster With Local Data/terraform/quickstart.sh
+++ Spread-Cluster With Local Data/terraform/quickstart.sh
@@ -3,11 +3,11 @@
 sudo apt-get update
 sudo apt-get install -y wget ansible git
 # Get Quobyte Ansible playbooks
-git clone git@github.com/quobyte/quobyte-ansible.git
+git clone git@github.com:quobyte/quobyte-ansible.git
 # Get presales playbooks
 git clone git@github.com:jan379/quobyte-presales.git
-git config --global user.email "jan.peschke@quobyte.com"
-git config --global user.name "Jan Peschke"
+#git config --global user.email "jan.peschke@quobyte.com"
+#git config --global user.name "Jan Peschke"
 
 # Let's start with a valid license right from the beginning
 grep "fill in a valid Quobyte license key" ansible-vars && exit 1
