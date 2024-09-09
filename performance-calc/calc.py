e95e145143e5ac7f2d1551277fb9c5fd8ff877eb
Jan Peschke <jan.peschke@quobyte.com>
2024-09-09 14:52:22 +0200

    Start Performance calc

---
+++ performance-calc/calc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Network parameters
+## We assume that switches are capable of handling the
+## load of each client/ server
+
+## The interface throughput of a single storage server
+backend_throughput_interfaces_gbs=25
+## The interface throughput of a single client machine
+frontend_throughput_interfaces_gbs=10
+
+# Client node specifics
+## The number of client machines
+number_clients=2
+
+# Server Node Specifics
+
+## Numer of storage nodes
+number_storagenodes=3
+## Number of devices per node
+number_storagenode_devices=24
+## Sustained throughput per device Mbit/s
+device_throughput_mbs=644 # 644=HDD, 3360=NVMe
+##
+host_throughput_device_mbs=number_storagenode_devices*device_throughput_mbs
+print(host_throughput_device_mbs)
