7590ff74790ab3108b6e62baa5718564ae3236f4
Jan Peschke <jan.peschke@quobyte.com>
2024-09-12 17:57:29 +0200

    first capacity calculations

--- performance-calc/calc.py
+++ performance-calc/calc.py
@@ -21,6 +21,8 @@ number_storagenodes=3
 number_storagenode_devices=24
 ## Sustained throughput per device Mbit/s
 device_throughput_mbs=644 # 644=HDD, 3360=NVMe
+device_capacity_GB=2200
+cluster_capacity_GB=number_storagenodes*number_storagenode_devices*device_capacity_GB
 
 ## EC code to use. 
 ec_codingstripes=3
@@ -35,4 +37,14 @@ host_throughput_device_mbs=number_storagenode_devices*device_throughput_mbs
 ## Sustained device throughput clusterwide
 cluster_throughput_device_mbs=number_storagenode_devices*device_throughput_mbs*number_storagenodes
 
-print("Welcome!")
+
+
+print("# Welcome!")
+print("")
+print("Your storage cluster consists of",number_storagenodes, "storagenodes.")
+print("")
+print("# Capacity")
+print("")
+print("Useable Capacity when using replication: %sGB" % (cluster_capacity_GB/3))
+print("Useable Capacity when using erasure coding EC%s+%s: %sGB" % (ec_datastripes,ec_codingstripes, (cluster_capacity_GB/((ec_datastripes+ec_codingstripes)/ec_datastripes))))
+print("RAW Capacity: %sGB" % (cluster_capacity_GB))
