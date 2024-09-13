3d35e1f1d30638e28d42e30df87f1b508434d831
Jan Peschke <jan.peschke@quobyte.com>
2024-09-13 14:44:12 +0200

    Clean up

--- performance-calc/calc.py
+++ performance-calc/calc.py
@@ -37,14 +37,26 @@ ec_codingstripes = int(config.get('storageconfig', 'ec_codingstripes'))
 # Replication stripe width is influencing performance
 replication_stripewidth = int(config.get('storageconfig', 'replication_stripewidth')) 
 
-## Sustained device throughput per storagenode
+# Sustained device throughput per storagenode
 host_throughput_device_mbs = number_storagenode_devices*device_throughput_mbs
 
-## Sustained device throughput clusterwide
+# Sustained device throughput clusterwide
 cluster_throughput_device_mbs = number_storagenode_devices*device_throughput_mbs*number_storagenodes
 
-# Overall cluster raw capacity
-cluster_capacity_GB = number_storagenodes*number_storagenode_devices*device_capacity_GB
+# Overall cluster raw capacity, raw
+cluster_capacity_raw_GB = round(number_storagenodes*number_storagenode_devices*device_capacity_GB, 2)
+cluster_capacity_raw_TB = round(cluster_capacity_raw_GB / 1024, 2)
+cluster_capacity_raw_PB = round(cluster_capacity_raw_TB / 1024, 2)
+
+# Overall cluster raw capacity, replicated
+cluster_capacity_repl_GB = round((cluster_capacity_raw_GB)/3, 2)
+cluster_capacity_repl_TB = round(cluster_capacity_repl_GB / 1024, 2)
+cluster_capacity_repl_PB = round(cluster_capacity_repl_TB / 1024, 2)
+
+# Overall cluster raw capacity, EC 
+cluster_capacity_ec_GB = round((cluster_capacity_raw_GB)/((ec_datastripes+ec_codingstripes)/ec_datastripes), 2)
+cluster_capacity_ec_TB = round(cluster_capacity_ec_GB / 1024, 2)
+cluster_capacity_ec_PB = round(cluster_capacity_ec_TB / 1024, 2)
 
 
 print("")
@@ -55,12 +67,12 @@ print("")
 print("## Capacity")
 print("")
 print("### Capacity RAW:")
-print("%sGB\t | %sTB\t | %sPB" % (round(cluster_capacity_GB,2), round(cluster_capacity_GB/1024,2), round(cluster_capacity_GB/1024/1024,2)))
-print("")
-print("### Capacity usable (3x replicated):")
-print("%sGB\t | %sTB\t | %sPB" % (round(cluster_capacity_GB/3, 2), round(cluster_capacity_GB/3/1024, 2), round(cluster_capacity_GB/3/1024/1024, 2)))
+print("%sGB\t | %sTB\t | %sPB" % (cluster_capacity_raw_GB, cluster_capacity_raw_TB, cluster_capacity_raw_PB))
 print("")
 print("### Capacity usable ( EC %s+%s):" % (ec_datastripes,ec_codingstripes))
-print("%sGB\t | %sTB\t | %sPB" % (round(cluster_capacity_GB/((ec_datastripes+ec_codingstripes)/ec_datastripes),2 ), round(cluster_capacity_GB/((ec_datastripes+ec_codingstripes)/ec_datastripes)/1024, 2), round(cluster_capacity_GB/((ec_datastripes+ec_codingstripes)/ec_datastripes)/1024/1024, 2)))
+print("%sGB\t | %sTB\t | %sPB" % (cluster_capacity_ec_GB, cluster_capacity_ec_TB, cluster_capacity_ec_PB))
+print("")
+print("### Capacity usable (3x replicated):")
+print("%sGB\t | %sTB\t | %sPB" % (cluster_capacity_repl_GB, cluster_capacity_repl_TB, cluster_capacity_repl_PB))
 print("")
 
