92a96254ad1dc9e24904fb169e4bf8c23aa01272
Jan Peschke <jan.peschke@quobyte.com>
2024-09-25 12:29:54 +0200

    Added calculation for single stream performance, replication.

--- performance-calc/calc.py
+++ performance-calc/calc.py
@@ -64,24 +64,41 @@ cluster_capacity_ec_PB = round(cluster_capacity_ec_TB / 1024, 2)
 # One upper throughput boundary is the overall device throughput capacity
 cluster_device_throughput_capacity = number_storagenodes * number_storagenode_devices * device_throughput_mbs
 
+# Network backend capacity
+# One upper throughput boundary is the overall storage network throughput capacity
+cluster_network_throughput_capacity = number_storagenodes * storagenode_nic_mbs
+
+# Network frontend capacity
+# One upper throughput boundary is the overall client network throughput capacity
+client_network_throughput_capacity = number_clients * client_nic_mbs 
+
 # Single client performance
 ## One client can write one stripe as fast as one storage device can write (sustained).
-## If you use more stripes (EC or repl. + stripe_width > 1), you can multiply write thoughput.
-## Stripe width should not be larger than number_storagenodes.
-## Replication traffic needs 2/3 of available backend network capacity, so it will max out at that value.
-## A single client will never be faster than client_nic_gbs.
-## A single client is also limited by overall device throughput. 
-replication_inputstream = device_throughput_mbs * replication_stripewidth
-# We will need two times the ingress bandwidth on a storage node for replication/ egress traffic
-replication_bandwidth = replication_inputstream * 2
-# We will be limited by outbound traffic from first dataservice to two replicas, because our 
-# replica sets should be written to other machines / failure domains.
-replication_limit = storagenode_nic_mbs / replication_bandwidth
-# We will have 1/3 of device throughput capacity available when using 3 x replication
-replication_cluster_throughput_capacity = round(cluster_device_throughput_capacity / 3)
-single_client_write_throughput_replicated_mbs = min(replication_inputstream, client_nic_mbs, replication_cluster_throughput_capacity, replication_limit)
+## If you use more stripes (EC or repl. + stripe_width > 1), you can multiply write throughput.
+## Placement algorithm will try to place data as distributed as possible, i.e. a file with 3x replication and 
+## stripe_width 2 will be distributed across devices on 6 nodes. Frontend and replication traffic will not interfere 
+## on such a cluster. But if a cluster is smaller (< replication factor * stripe_width), frontend traffic and replication 
+## traffic will share the same network interface. As a consequence this NIC can become a bottleneck.
+
+# Replication
+## A single client replication is limited by client_nic_mbs.
+## A single client replication is limited by cluster_device_throughput_capacity minus replication_device_throughput_requirements 
+replication_device_throughput_requirements = ((cluster_device_throughput_capacity / 3) * 2)
+cluster_device_replication_capacity = cluster_device_throughput_capacity - replication_device_throughput_requirements
+## A single client replication is limited by cluster_network_throughput_capacity minus replication_network_throughput_requirements. 
+replication_network_throughput_requirements = ((cluster_network_throughput_capacity / 3) * 2)
+cluster_network_replication_capacity = cluster_network_throughput_capacity - replication_network_throughput_requirements
+## A single client replication has the expected throughput of "device_throughput_mbs * replication_stripewidth".
+single_client_replicated_striped = device_throughput_mbs * replication_stripewidth
+single_client_write_throughput_replicated_mbs = min(client_nic_mbs, cluster_device_replication_capacity, cluster_network_replication_capacity, single_client_replicated_striped)
 single_client_write_throughput_replicated_MBs = round(single_client_write_throughput_replicated_mbs / 8, 2)
 
+# Erasure Coding
+## A single client EC is limited by client host network capacity minus coding_stripe_bandwidth.
+## That is (client_nic_mbs / (ec_datastripes + ec_codingstripes)) * ec_datastripes), 
+## A single client EC is limited by cluster_network_throughput_capacity.
+## A single client EC is limited by cluster_device_throughput_capacity.
+## A single  client EC has the expected throughput of "device_throughput_mbs * data stripe count"
 
 print("")
 print("# Welcome!")
@@ -91,20 +108,20 @@ print("")
 print("## Capacity")
 print("")
 print("### Capacity RAW:")
-print("%sGB\t | %sTB\t | %sPB" % (cluster_capacity_raw_GB, cluster_capacity_raw_TB, cluster_capacity_raw_PB))
+print("%s GB\t | %s TB\t | %s PB" % (cluster_capacity_raw_GB, cluster_capacity_raw_TB, cluster_capacity_raw_PB))
 print("")
 print("### Capacity usable ( EC %s+%s):" % (ec_datastripes,ec_codingstripes))
-print("%sGB\t | %sTB\t | %sPB" % (cluster_capacity_ec_GB, cluster_capacity_ec_TB, cluster_capacity_ec_PB))
+print("%s GB\t | %s TB\t | %s PB" % (cluster_capacity_ec_GB, cluster_capacity_ec_TB, cluster_capacity_ec_PB))
 print("")
 print("### Capacity usable (3x replicated):")
-print("%sGB\t | %sTB\t | %sPB" % (cluster_capacity_repl_GB, cluster_capacity_repl_TB, cluster_capacity_repl_PB))
+print("%s GB\t | %s TB\t | %s PB" % (cluster_capacity_repl_GB, cluster_capacity_repl_TB, cluster_capacity_repl_PB))
 print("")
 
 print("")
 print("## Performance")
 print("")
-print("### Theoretical max. single client performance (MByte/s):")
-print(single_client_write_throughput_replicated_MBs)
+print("### Theoretical max. single client/ single stream performance (data stored 3x replicated):")
+print("%s MB/s" % (single_client_write_throughput_replicated_MBs))
 print("")
 print("### Theoretical max. multi client performance:")
 print("")
