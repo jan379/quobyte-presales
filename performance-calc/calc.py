cbad61df05b2af19d60f4d604f4010b1cd2e9d8c
Jan Peschke <jan.peschke@quobyte.com>
2024-10-02 11:06:24 +0200

    adjust vaules

--- performance-calc/calc.py
+++ performance-calc/calc.py
@@ -22,11 +22,11 @@ except:
     print("could not read configfile %s" % (configfile))
 
 client_nic_gbs = float(config.get('clients', 'capacity_nic_gbs'))
-client_nic_mbs = float(client_nic_gbs*1024)
+client_nic_mbs = float(client_nic_gbs*1000)
 number_clients = int(config.get('clients', 'number_clients'))
 
 storagenode_nic_gbs = float(config.get('storagenodes', 'capacity_nic_gbs'))
-storagenode_nic_mbs = float(storagenode_nic_gbs * 1024)
+storagenode_nic_mbs = float(storagenode_nic_gbs * 1000)
 number_storagenodes = int(config.get('storagenodes', 'number_nodes'))
 number_storagenode_devices = int(config.get('storagenodes', 'number_devices'))
 device_throughput_mbs = int(config.get('storagenodes', 'device_throughput_mbs'))
@@ -49,18 +49,18 @@ cluster_throughput_device_mbs = host_throughput_device_mbs * number_storagenodes
 
 # Overall cluster raw capacity, raw
 cluster_capacity_raw_GB = round(number_storagenodes*number_storagenode_devices*device_capacity_GB, 2)
-cluster_capacity_raw_TB = round(cluster_capacity_raw_GB / 1024, 2)
-cluster_capacity_raw_PB = round(cluster_capacity_raw_TB / 1024, 2)
+cluster_capacity_raw_TB = round(cluster_capacity_raw_GB / 1000, 2)
+cluster_capacity_raw_PB = round(cluster_capacity_raw_TB / 1000, 2)
 
 # Overall cluster raw capacity, replicated
 cluster_capacity_repl_GB = round((cluster_capacity_raw_GB)/replication_factor, 2)
-cluster_capacity_repl_TB = round(cluster_capacity_repl_GB / 1024, 2)
-cluster_capacity_repl_PB = round(cluster_capacity_repl_TB / 1024, 2)
+cluster_capacity_repl_TB = round(cluster_capacity_repl_GB / 1000, 2)
+cluster_capacity_repl_PB = round(cluster_capacity_repl_TB / 1000, 2)
 
 # Overall cluster raw capacity, EC 
 cluster_capacity_ec_GB = round((cluster_capacity_raw_GB)/((ec_datastripes+ec_codingstripes)/ec_datastripes), 2)
-cluster_capacity_ec_TB = round(cluster_capacity_ec_GB / 1024, 2)
-cluster_capacity_ec_PB = round(cluster_capacity_ec_TB / 1024, 2)
+cluster_capacity_ec_TB = round(cluster_capacity_ec_GB / 1000, 2)
+cluster_capacity_ec_PB = round(cluster_capacity_ec_TB / 1000, 2)
 
 # Device performance
 # One upper throughput boundary is the overall device throughput capacity
