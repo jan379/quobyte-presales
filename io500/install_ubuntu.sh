0b60d752507e2be8a36f88cc3ac0a0f2b42a1ebc
Jan Peschke <jan.peschke@quobyte.com>
2025-01-30 11:05:18 +0000

    Get mpi up and running, create client list etc

--- io500/install_ubuntu.sh
+++ io500/install_ubuntu.sh
@@ -1,4 +1,7 @@
 # install requirements
+cd /quobyte/io500-bin/
+mkdir io500
+cd io500
 sudo apt install build-essential dh-autoreconf pkg-config m4 libtool automake openmpi-bin mpi-default-dev
 # clone main branch
 git clone https://github.com/IO500/io500.git
