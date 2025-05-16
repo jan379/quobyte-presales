bbadbe3651f0c02bd11e851d674433f573e7ee03
Jan Peschke <jan.peschke@quobyte.com>
2025-05-16 13:10:44 +0000

    isc24 tag is broken by changed pfind (https://github.com/VI4IO/pfind/commit/aaba722a178d38799125a271a7f554d004713363)

--- io500/install_ubuntu.sh
+++ io500/install_ubuntu.sh
@@ -7,7 +7,7 @@ sudo apt install build-essential dh-autoreconf pkg-config m4 libtool automake op
 git clone https://github.com/IO500/io500.git
 cd io500
 # check out to sc24 tag
-git checkout io500-sc24
+##git checkout io500-sc25
 ./prepare.sh
 make
 
