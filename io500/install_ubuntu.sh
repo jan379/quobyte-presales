c76387f3c321e60a3df0c6e6498462c7a06c614d
Jan Peschke <jan.peschke@quobyte.com>
2025-07-29 11:13:00 +0000

    Move to multi tenant mount

--- io500/install_ubuntu.sh
+++ io500/install_ubuntu.sh
@@ -1,7 +1,5 @@
 # install requirements
-cd /quobyte/io500-bin/
-mkdir io500
-cd io500
+cd /quobyte/io500/io500-bin/
 sudo apt install build-essential dh-autoreconf pkg-config m4 libtool automake openmpi-bin mpi-default-dev
 # clone main branch
 git clone https://github.com/IO500/io500.git
@@ -11,9 +9,11 @@ cd io500
 ./prepare.sh
 make
 
-/quobyte/io500-bin/io500/io500/io500 --list > /quobyte/io500-bin/fullconfig.ini
-sed -i s#./datafiles#/quobyte/io500-data#g /quobyte/io500-bin/fullconfig.ini 
+# hierarchy is: /mountpoint/tenant/volumename/io500-repo/io500-binary
+/quobyte/io500/io500-bin/io500/io500 --list > /quobyte/io500/io500-bin/fullconfig.ini
+# hierarchy is: /mountpoint/tenant/volumename/
+sed -i s#./datafiles#/quobyte/io500/io500-data#g /quobyte/io500/io500-bin/fullconfig.ini 
 
-echo mpirun  --hostfile /quobyte/io500-bin/io500-clients.list /quobyte/io500-bin/io500/io500/io500 /quobyte/io500-bin/fullconfig.ini
+echo mpirun  --hostfile /quobyte/io500/io500-bin/io500-clients.list /quobyte/io500/io500-bin/io500/io500 /quobyte/io500-bin/fullconfig.ini
 
 
